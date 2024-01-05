feab1fb2e96b6f2541d8d45e93f6fe287ff1934a
Roberto P. Rubio <outtakes_handgun090@familiapm.com>
2024-01-05 18:25:10 +0100

    Añade librería de ayuda de google-maps

---
+++ app/utils/google_maps.py
@@ -0,0 +1,30 @@
+from enum import Enum
+
+import subprocess
+import re
+
+class GetLocationResultType(Enum):
+    NO_ERROR = 0,
+    CANT_DECODE_URL = 1,
+    UNSUPPORTED_URL_FORMAT = 2
+
+class GoogleMapsUtils():
+    @staticmethod
+    def get_location(url):
+        if url.startswith('https://maps.app.goo.gl/'):
+            stdout, error = subprocess.Popen([ 'curl', '-Ls', '-o', '/dev/null', '-w', '%{url_effective}', url ], stdout = subprocess.PIPE, stderr = subprocess.PIPE).communicate()
+            if stdout is None:
+                return GetLocationResultType.CANT_DECODE_URL, None
+            else:
+                url = stdout.decode()
+        if url.startswith('https://www.google.com/maps/'):
+            match = re.search("@[^,]+,[^,]+,", url)
+            if match is not None:
+                location = match.group(0)
+                return GetLocationResultType.NO_ERROR, location[1:len(location) - 1]
+            match = re.search("ll=[^,]+,[^,]+,", url)
+            if match is not None:
+                location = match.group(0)
+                return GetLocationResultType.NO_ERROR, location[3:len(location) - 1]
+        return GetLocationResultType.UNSUPPORTED_URL_FORMAT, None
+
